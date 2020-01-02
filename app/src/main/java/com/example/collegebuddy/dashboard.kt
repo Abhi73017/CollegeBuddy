678e835927900431db995e9ee00e90d81b36fe1f
Abhi73017 <52728167+Abhi73017@users.noreply.github.com>
2020-01-02 12:14:03 +0530

    bottom navigation with fragments

--- app/src/main/java/com/example/collegebuddy/dashboard.kt
+++ app/src/main/java/com/example/collegebuddy/dashboard.kt
@@ -1,45 +1,29 @@
 package com.example.collegebuddy
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.annotation.NonNull
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import kotlinx.android.synthetic.main.activity_dashboard.*
 
 class dashboard : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-
-        nav_view.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.navigation_home -> {
-
-                }
-                R.id.navigation_contact_us -> {
-
-                    val intent1 = Intent(this, contact_us::class.java)
-                    startActivity(intent1)
-                }
-                R.id.navigation_my_account -> {
-                    val intent2 = Intent(this, my_account::class.java)
-                    startActivity(intent2)
-                }
-                R.id.navigation_more -> {
-                    val intent3 = Intent(this, more::class.java)
-                    startActivity(intent3)
-                }
-            }
-            return@setOnNavigationItemSelectedListener true
-        }
-
-
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_contact_us, R.id.navigation_my_account, R.id.navigation_more
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
