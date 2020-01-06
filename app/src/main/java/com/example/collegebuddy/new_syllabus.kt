826b2f735a37905543e4271d7869405dbd2f1523
Abhi73017 <52728167+Abhi73017@users.noreply.github.com>
2020-01-06 19:37:47 +0530

    All buttons are changed and two activities are created

---
+++ app/src/main/java/com/example/collegebuddy/new_syllabus.kt
@@ -0,0 +1,12 @@
+package com.example.collegebuddy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class new_syllabus : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_syllabus)
+    }
+}
---
+++ app/src/main/java/com/example/collegebuddy/old_syllabus.kt
@@ -0,0 +1,12 @@
+package com.example.collegebuddy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class old_syllabus : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_old_syllabus)
+    }
+}
