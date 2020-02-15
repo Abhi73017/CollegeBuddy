2ba4959ba9308e04f853b6261e24ca4d5168bb2b
Abhi73017 <52728167+Abhi73017@users.noreply.github.com>
2020-02-15 14:04:50 +0530

    Question bank layout file has been created.

--- app/src/main/java/com/abhishek/collegebuddy/question_bank_package/question_bank.kt
+++ app/src/main/java/com/abhishek/collegebuddy/question_bank_package/question_bank.kt
@@ -2,12 +2,71 @@ package com.abhishek.collegebuddy.question_bank_package
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
 import com.abhishek.collegebuddy.R
+import kotlinx.android.synthetic.main.activity_question_bank.*
 
 class question_bank : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question_bank)
+
+        showbtn.setOnClickListener {
+            if (btn1layout.isVisible){
+                btn1layout.visibility = View.GONE
+
+            }
+            else{
+                btn1layout.visibility = View.VISIBLE
+
+
+            }
+        }
+
+//        Button2
+        showbtn2.setOnClickListener {
+            if (btn2layout.isVisible){
+                btn2layout.visibility = View.GONE
+            }
+            else{
+                btn2layout.visibility = View.VISIBLE
+
+            }
+        }
+
+//        Button3
+        showbtn3.setOnClickListener {
+            if (btn3layout.isVisible){
+                btn3layout.visibility = View.GONE
+            }
+            else{
+                btn3layout.visibility = View.VISIBLE
+
+            }
+        }
+
+//        Button4
+        showbtn4.setOnClickListener {
+            if (btn4layout.isVisible){
+                btn4layout.visibility = View.GONE
+            }
+            else{
+                btn4layout.visibility = View.VISIBLE
+
+            }
+        }
+
+//      Button5
+        showbtn5.setOnClickListener {
+            if (btn5layout.isVisible){
+                btn5layout.visibility = View.GONE
+            }
+            else{
+                btn5layout.visibility = View.VISIBLE
+
+            }
+        }
     }
 }
